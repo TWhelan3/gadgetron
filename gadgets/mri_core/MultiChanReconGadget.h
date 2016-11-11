0212f194695e8842780987ca90afac9fee58e992
Tim Whelan <twhelan5@uwo.ca>
2016-11-11 11:57:33 -0500

    Adds multi-channel reconstruction

---
+++ gadgets/mri_core/MultiChanReconGadget.h
@@ -0,0 +1,24 @@
+#ifndef MULTICHANRECONGADGET_H
+#define MULTICHANRECONGADGET_H
+
+#include "Gadget.h"
+#include "gadgetron_mricore_export.h"
+#include "SimpleReconGadget.h"
+
+#include "mri_core_data.h"
+
+namespace Gadgetron{
+
+  class EXPORTGADGETSMRICORE MultiChanReconGadget : 
+  public SimpleReconGadget
+    {
+    public:
+      GADGET_DECLARE(MultiChanReconGadget)
+     // MultiChanReconGadget();
+	
+    protected:
+      virtual int process(GadgetContainerMessage<IsmrmrdReconData>* m1);
+     // long long image_counter_;
+    };
+}
+#endif //MULTICHANRECONGADGET_H
